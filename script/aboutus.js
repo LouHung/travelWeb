5cbe7018546497a1ecbfd043a9178e9869e60f3a
louhung <quochung51199@gmail.com>
2021-11-22 13:17:22 +0700

    ok

--- script/aboutus.js
+++ script/aboutus.js
@@ -1,7 +1,8 @@
-const imgBackground = $(".body-staff-background")
+// const imgBackground = $(".body-staff-background")
 
 window.addEventListener("scroll",function(){
     let offset = window.pageYOffset
-    console.log(offset)
-    imgBackground.style.backgroundPositionY = `calc(-20em + ${offset*0.1}px)`
+
+    // imgBackground.style.backgroundPositionY = `calc(-20em + ${offset*0.1}px)`;
+    document.documentElement.style.setProperty('--position-pic-flow',`calc(-37em + ${offset*0.2}px)`)
 })
