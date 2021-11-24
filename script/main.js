6d8cc9737b3f952937c9ec544a2099bdfb17bbde
louhung <quochung51199@gmail.com>
2021-11-24 15:41:33 +0700

    ok

--- script/main.js
+++ script/main.js
@@ -1,39 +1,83 @@
-// const $ = document.querySelector.bind(document)
-// const $$ = document.querySelectorAll.bind(document)
-// const srcVideo = $('.packages__video-content__srcVideo')
-// const body = $('body')
-// const playVideoBtn = $('.packages__video-content')
-// const mainPicContainer = $('.main-pic')
-// const video = $('.packages__video-content__srcVideo-item')
-// const navBtn = $('.header__logo-brand__navBtn')
-// const listNavSide = $('.header__navbar-flow__container')
-// const closeNavBtn = $('.header__navbar-flow__iconClose')
-// const media750px = window.matchMedia("(max-width:750px)")
+
 let length = 0
-let btnPrev = $('.main-pic__switchPageIcon__prev')
-let btnNext = $('.main-pic__switchPageIcon__next')
-let mainPicArr=['asset/img/slide-01.jpg','asset/img/slide-02.jpg']
+const btnPrev = $('.main-pic__switchPageIcon__prev')
+const btnNext = $('.main-pic__switchPageIcon__next')
 let mainPic = $('.main-pic__item')
+let mainPicArr=['asset/img/slide-01.jpg','asset/img/slide-02.jpg']
 mainPic.style.backgroundImage = `url(${mainPicArr[0]})`
 
+let contentMainPic= ` 
+    
+<div class="grid2 main-pic__item-container-form">
+<div class="main-pic__item-container">
+   <div class="main-pic__item__banner">
+       <div class="main-pic__item__banner-item">
+           Enjoy <span>Your</span> Holiday
+       </div>
+       <div class="main-pic__item__banner-item">
+           with Our <span>Agency</span>
+       </div>
+   </div>
 
+   <div class="main-pic__item__description">
+       <div class="main-pic__item__description-item">
+           <i class="main-pic__item__description-item__star fas fa-star"></i>
+           <span>spectacular things to do best </span>
+       </div>
+       <div class="main-pic__item__description-item">
+           <i class="main-pic__item__description-item__star fas fa-star"></i>
+           <span>local guide available</span>
+       </div>
+       <div class="main-pic__item__description-item">
+           <i class="main-pic__item__description-item__star fas fa-star"></i>
+           <span>all inclusive family holiday for all season</span>
+       </div>
+   </div>
 
+   <div class="btn main-pic__item__btn">
+       buy the tour
+       <span class="main-pic__item__btn__discount">$25</span>
+   </div>
+</div>
+<div class="main-pic__item__btn2 btn">
+<a href="../travelWeb/packages.html">buy the tour</a>
+</div>  
+
+</div>`
 
 
 
 run()
 function run() {
     mainPicSwitcher()
+    resizeMainPic()
 }
-var barslide = $(".bar_slide")
+
+function setLineStop(){
+    document.documentElement.style.setProperty('--transition-slideBar',"none")
+    document.documentElement.style.setProperty('--width_slideBar',"0%")
+}
+function setSlideStart(){
+    document.documentElement.style.setProperty('--transition-slideBar',"all 8s linear")
+        document.documentElement.style.setProperty('--width_slideBar',"100%")
+}
+
+function resetLine(){
+    document.documentElement.style.setProperty('--transition-slideBar',"none")
+    document.documentElement.style.setProperty('--width_slideBar',"0%")
+    setTimeout(function(){
+        document.documentElement.style.setProperty('--transition-slideBar',"all 8s linear")
+        document.documentElement.style.setProperty('--width_slideBar',"100%")
+    },500)//don't know why if I don't put setTimeout here it doesn't work accurately
+}
 
 function mainPicSwitcher() {
-    document.documentElement.style.setProperty('--width_slideBar',"100%")
+   
+    resetLine()
+   
     
     let autoplay=setInterval(()=>{
-        document.documentElement.style.setProperty('--width_slideBar',"0%")
-        document.documentElement.style.setProperty('--transition-slideBar',"none")
-        
+        setLineStop()
 
         length++
         if(length >= mainPicArr.length){
@@ -46,54 +90,18 @@ function mainPicSwitcher() {
                 mainPic.style.backgroundImage = `url(${mainPicArr[length]})`
             }else{
             mainPic.style.backgroundImage = `url(${mainPicArr[length]})`
-            mainPic.innerHTML = ` 
-            
-    
-            <div class="grid2 main-pic__item-container-form">
-                <div class="main-pic__item-container">
-                   <div class="main-pic__item__banner">
-                       <div class="main-pic__item__banner-item">
-                           Enjoy <span>Your</span> Holiday
-                       </div>
-                       <div class="main-pic__item__banner-item">
-                           with Our <span>Agency</span>
-                       </div>
-                   </div>
-    
-                   <div class="main-pic__item__description">
-                       <div class="main-pic__item__description-item">
-                           <i class="main-pic__item__description-item__star fas fa-star"></i>
-                           <span>spectacular things to do best </span>
-                       </div>
-                       <div class="main-pic__item__description-item">
-                           <i class="main-pic__item__description-item__star fas fa-star"></i>
-                           <span>local guide available</span>
-                       </div>
-                       <div class="main-pic__item__description-item">
-                           <i class="main-pic__item__description-item__star fas fa-star"></i>
-                           <span>all inclusive family holiday for all season</span>
-                       </div>
-                   </div>
-    
-                   <div class="btn main-pic__item__btn">
-                       buy the tour
-                       <span class="main-pic__item__btn__discount">$25</span>
-                   </div>
-                </div>
-                <div class="main-pic__item__btn2 btn">
-                <a href="../travelWeb/packages.html">buy the tour</a>
-            </div>  
-    
-           </div>`
+            mainPic.innerHTML = contentMainPic
             }
-            document.documentElement.style.setProperty('--transition-slideBar',"all 8s linear")
-        document.documentElement.style.setProperty('--width_slideBar',"100%")
+            setSlideStart()
         },300)
     },9000)
+   
+   
+function btnSwitcher() {
 
+}
     btnNext.onclick = () =>{
-    document.documentElement.style.setProperty('--width_slideBar',"0%")
-    document.documentElement.style.setProperty('--transition-slideBar',"none")
+        setLineStop()
 
         length++
         if(length >= mainPicArr.length){
@@ -101,95 +109,20 @@ function mainPicSwitcher() {
         }
         setTimeout(function(){
             mainPic.style.backgroundImage = `url(${mainPicArr[length]})`
-            mainPic.innerHTML = ` 
-    
-            <div class="grid2 main-pic__item-container-form">
-            <div class="main-pic__item-container">
-               <div class="main-pic__item__banner">
-                   <div class="main-pic__item__banner-item">
-                       Enjoy <span>Your</span> Holiday
-                   </div>
-                   <div class="main-pic__item__banner-item">
-                       with Our <span>Agency</span>
-                   </div>
-               </div>
-
-               <div class="main-pic__item__description">
-                   <div class="main-pic__item__description-item">
-                       <i class="main-pic__item__description-item__star fas fa-star"></i>
-                       <span>spectacular things to do best </span>
-                   </div>
-                   <div class="main-pic__item__description-item">
-                       <i class="main-pic__item__description-item__star fas fa-star"></i>
-                       <span>local guide available</span>
-                   </div>
-                   <div class="main-pic__item__description-item">
-                       <i class="main-pic__item__description-item__star fas fa-star"></i>
-                       <span>all inclusive family holiday for all season</span>
-                   </div>
-               </div>
-
-               <div class="btn main-pic__item__btn">
-                   buy the tour
-                   <span class="main-pic__item__btn__discount">$25</span>
-               </div>
-            </div>
-            <div class="main-pic__item__btn2 btn">
-            <a href="../travelWeb/packages.html">buy the tour</a>
-        </div>  
-
-       </div>`
+            mainPic.innerHTML = contentMainPic
         },300)
     clearInterval(autoplay)
     }
 
     btnPrev.onclick = () =>{
-    document.documentElement.style.setProperty('--width_slideBar',"0%")
-    document.documentElement.style.setProperty('--transition-slideBar',"none")
+        setLineStop()
         length--
         if(length < 0){
             length = mainPicArr.length-1
         }
         setTimeout(function(){
             mainPic.style.backgroundImage = `url(${mainPicArr[length]})`
-            mainPic.innerHTML = `  
-    
-            <div class="grid2 main-pic__item-container-form">
-            <div class="main-pic__item-container">
-               <div class="main-pic__item__banner">
-                   <div class="main-pic__item__banner-item">
-                       Enjoy <span>Your</span> Holiday
-                   </div>
-                   <div class="main-pic__item__banner-item">
-                       with Our <span>Agency</span>
-                   </div>
-               </div>
-
-               <div class="main-pic__item__description">
-                   <div class="main-pic__item__description-item">
-                       <i class="main-pic__item__description-item__star fas fa-star"></i>
-                       <span>spectacular things to do best </span>
-                   </div>
-                   <div class="main-pic__item__description-item">
-                       <i class="main-pic__item__description-item__star fas fa-star"></i>
-                       <span>local guide available</span>
-                   </div>
-                   <div class="main-pic__item__description-item">
-                       <i class="main-pic__item__description-item__star fas fa-star"></i>
-                       <span>all inclusive family holiday for all season</span>
-                   </div>
-               </div>
-
-               <div class="btn main-pic__item__btn">
-                   buy the tour
-                   <span class="main-pic__item__btn__discount">$25</span>
-               </div>
-            </div>
-            <div class="main-pic__item__btn2 btn">
-            <a href="../travelWeb/packages.html">buy the tour</a>
-        </div>  
-
-       </div>`
+            mainPic.innerHTML = contentMainPic
         },300)
         clearInterval(autoplay)
     
@@ -197,19 +130,21 @@ function mainPicSwitcher() {
     // document.documentElement.style.setProperty('--width_slideBar',"0%")
 
 }
-let width = window.innerWidth;
-console.log(width);
-var heightMainPic = $('.main-pic__item')
-heightMainPic.style.height=`${width}px`
-var onresize = function() {
-     width = window.innerWidth;
-    // width%2==0?numMinus++:0
-    // btnMainPic.style.padding
 
-    heightMainPic.style.height= `${width}px`
-    // console.log(btnMainPic)
+
+//resize mainPic when resize the window
+function resizeMainPic(){
+    let width = window.innerWidth;
+    let heightMainPic = $('.main-pic__item')
+    heightMainPic.style.height=`${width}px`
+    let onresize = function() {
+         width = window.innerWidth;
+    
+        heightMainPic.style.height= `${width}px`
+    }
+    window.onresize = onresize
 }
-window.onresize = onresize
+
 
 
 
